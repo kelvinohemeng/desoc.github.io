3bbb63a5bb955e1090de3e03bb4a78239e91fd83
kelvin Ohemeng <kelvinohemeng59@gmail.com>
2022-12-03 02:47:48 +0000

    There's a bug
    I I can't fix it at the moment 😪

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,6 +1,9 @@
 const navOpen = document.getElementById("nav_open")
 const navCloseContainer = document.getElementById("nav_close")
+const navLinks = document.querySelectorAll(".nav-links")
 const navCloseIcon = document.getElementById("close_nav")
+
+console.log(navLinks)
 
 navOpen.addEventListener('click', ()=>{
     document.body.classList.add('deactivate-scroll')
@@ -10,7 +13,13 @@ navCloseIcon.addEventListener('click', ()=>{
     document.body.classList.remove('deactivate-scroll')
     navCloseContainer.style.display = 'none'
 })
-navCloseContainer.addEventListener('click', ()=>{
-    document.body.classList.remove('deactivate-scroll')
-    navCloseContainer.style.display = 'none'
-})
+
+for (let i = 0; i < navLinks.length; i++) {
+    const navLink = navLinks[i];
+    navLink.addEventListener('click', ()=>{
+        document.body.classList.remove('deactivate-scroll')
+        navCloseContainer.style.display = 'none'
+    })
+
+    
+}
