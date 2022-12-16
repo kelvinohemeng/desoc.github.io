1836b8077b63e18c9ad9ce51bc20020bd290147c
kelvin Ohemeng <kelvinohemeng59@gmail.com>
2022-12-16 14:14:38 +0000

    remove deactivat scroll

--- src/js/nav.js
+++ src/js/nav.js
@@ -7,13 +7,13 @@ const signUpInner = document.querySelector(".sign_container")
 
 document.querySelector(".cta_nav").addEventListener('click', (e)=>{
     signUp.style.display = "flex"
-    document.body.classList.add('deactivate-scroll')
+    // document.body.classList.add('deactivate-scroll')
     e.stopPropagation()
     // signUp.classList.add("open-signup")
 })
 document.querySelector(".b-signup").addEventListener('click', (e)=>{
     signUp.style.display = "none"
-    document.body.classList.remove('deactivate-scroll')
+    // document.body.classList.remove('deactivate-scroll')
 })
 
 signUpInner.addEventListener('click', (e)=>{
@@ -23,18 +23,18 @@ signUpInner.addEventListener('click', (e)=>{
 })
 
 navOpen.addEventListener('click', ()=>{
-    document.body.classList.add('deactivate-scroll')
+    // document.body.classList.add('deactivate-scroll')
     navCloseContainer.style.display = 'flex'
 })
 navCloseIcon.addEventListener('click', ()=>{
-    document.body.classList.remove('deactivate-scroll')
+    // document.body.classList.remove('deactivate-scroll')
     navCloseContainer.style.display = 'none'
 })
 
 for (let i = 0; i < navLinks.length; i++) {
     const navLink = navLinks[i];
     navLink.addEventListener('click', ()=>{
-        document.body.classList.remove('deactivate-scroll')
+        // document.body.classList.remove('deactivate-scroll')
         navCloseContainer.style.display = 'none'
     })
 }
